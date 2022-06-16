a4d818971123c5ee524f26438edb13b200971c2e
Clay Aldas <clayaldas@gmail.com>
2022-06-16 18:43:52 -0500

    Proyecto creado

---
+++ app/src/main/java/fisei/uta/ec/app_kotlin_003/MainActivity.kt
@@ -0,0 +1,11 @@
+package fisei.uta.ec.app_kotlin_003
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
