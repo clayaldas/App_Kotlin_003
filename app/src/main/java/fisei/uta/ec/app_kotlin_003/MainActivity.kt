4719a83b809f0fa454561fc0163acb7ae75ea6bb
Clay Aldas <clayaldas@gmail.com>
2022-06-20 20:06:28 -0500

    Lunes 20 de junio

--- app/src/main/java/fisei/uta/ec/app_kotlin_003/MainActivity.kt
+++ app/src/main/java/fisei/uta/ec/app_kotlin_003/MainActivity.kt
@@ -11,5 +11,18 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // asignar los datos (Adapter) al control (Recycler)
+        binding.recycler.adapter = MoviesAdapter(
+            //cargar datos de prueba
+            listOf(
+                Movie("Titulo 1", "https://loremflickr.com/320/240?lock=1"),
+                Movie("Titulo 2", "https://loremflickr.com/320/240?lock=2"),
+                Movie("Titulo 3", "https://loremflickr.com/320/240?lock=3"),
+                Movie("Titulo 4", "https://loremflickr.com/320/240?lock=4"),
+                Movie("Titulo 5", "https://loremflickr.com/320/240?lock=5"),
+                Movie("Titulo 6", "https://loremflickr.com/320/240?lock=6")
+            )
+        )
     }
 }
--- app/src/main/java/fisei/uta/ec/app_kotlin_003/MoviesAdapter.kt
+++ app/src/main/java/fisei/uta/ec/app_kotlin_003/MoviesAdapter.kt
@@ -1,25 +1,68 @@
 package fisei.uta.ec.app_kotlin_003
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import fisei.uta.ec.app_kotlin_003.databinding.ViewMovieItemBinding
 
 class MoviesAdapter (private val movies: List<Movie>) : RecyclerView.Adapter<MoviesAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+    // primer metodo
+    /*al view = LayoutInflater.
+                    from(parent.context).
+                    inflate(R.layout.view_movie_item, parent, false)
+
+        return ViewHolder(view)*/
+
+        // segundo metodo (usando binding)
+        val binding = ViewMovieItemBinding.inflate(LayoutInflater.from(parent.context),
+            parent, false)
+
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        // actualizar (enlazar) la fila cual referencia (el titulo y la imagen actual)
+        holder.bind(movies[position])
     }
 
     override fun getItemCount(): Int {
         return movies.size
     }
-
     // override fun getItemCount() = movies.size
 
-    class ViewHolder (view: View) : RecyclerView.ViewHolder (view)
+
+    // primer metodo
+    //class ViewHolder (view: View) : RecyclerView.ViewHolder (view)
+
+    // segundo metodo (usando binding)
+    class ViewHolder (private val binding: ViewMovieItemBinding) : RecyclerView.ViewHolder (binding.root) {
+
+        // recuperar una registro
+        fun bind (movie: Movie) {
+            // asignar el titulo y la imagen de la pelicula
+            binding.textViewTitle.text = movie.title
+
+            //cargar la pelicula del sitio web
+            Glide.with(binding.root.context).load(movie.cover).into(binding.imageViewCover)
+
+            /*
+            Glide
+                .with(binding.root.context)
+                .load(movie.cover)
+                .into(binding.imageViewCover)
+             */
+
+        }
+
+        /*
+        fun bind (movie: Movie) : Unit {
+            // asignar el titulo y la imagen de la pelicula
+        }
+         */
+    }
 }
 
